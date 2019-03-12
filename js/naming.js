b5d16c9c8f6d184b041a7a0e709a72e6dd7976a8
Nantipat Suwanwong <scemisnis@gmail.com>
2019-03-12 22:27:50 +0700

    htmlCommentProtection
    brutal statement not work at all

--- js/naming.js
+++ js/naming.js
@@ -21,14 +21,18 @@ $("#play").click(function(){
                 var aliases = data.Alias;
                 var alias = aliases[goRandom(aliases.length)];
                 var nickname=$("#nickname").val();
+                nickname=htmlCommentProtection(nickname);
                 console.log(alias);
                 console.log(age);
                 //var fullalias= '<h1>ฉายา</h1>'+alias+'<p>คำอธิบาย : <textarea>coming soon</textarea></p>';
-                var fullalias= '<h1>ฉายา</h1><h3>'+nickname+'</h3><h2>'+alias+'</h2><p>คำอธิบาย : coming soon</p>';
+                //var fullalias= '<h1>ฉายา</h1><h3>'+nickname+'</h3><h2>'+alias+'</h2><p>คำอธิบาย : coming soon</p>';
+                var fullalias= '<h1>ฉายา</h1><h3>'+nickname+'</h3><h2>'+alias+'</h2>';
                 $("#result").append(fullalias);
                 $(".result").toggle();
+                $("#play").toggle();
                 $("#play").attr("value"," Play ");
                 $("#result").fadeIn(5000,function(){
+                    $("#play").slideDown("slow");
                     $("#play").attr("value","Play Again");
                 });
             }else
@@ -64,48 +68,15 @@ function getSexType(sex) {
         return goRandom(2);
     }
 }
-
-/*uncomplicate functions */
-// $("#play").click(function () {
-//     /*button play*/
-//     if ($(this).val() == "Play") {
-//         $(".inform").toggle("slow");
-//         $.get("aliases.json", function (data, status) {
-//             if (status = "success") {
-//                 var sex = $("#sex").val();
-//                 var year = $("#year").val();
-//                 var nickname = $("#nickname").val();
-//                 var age = getAge(year);
-//                 var type = data.Alias[getSexType(sex)];
-//                 var alias = getAlias(type, age);
-//                 //console.log(type.identify+"-"+sex+"-"+age+"-"+year);
-//                 var tagAlias = '<p>' + nickname + '</p>' + '<h2>The</h2>' + '<p>' + alias + '</p><br>';
-//                 $("#result").append(tagAlias);
-//                 $("#result").fadeIn(5000,function(){
-//                     $("#play").attr("value","Play Again");
-//                 });
-//             } else {
-//                 console.log(status);
-//             }
-//         });
-//         $("#play").attr("value"," Play ");
-        
-//         //$(this).attr("value", "Play Again");
-//     } else if($(this).val()=="Play Again"){
-//         $("#result").empty();
-//         $("#result").toggle();
-//         $(".inform").toggle("slow");
-//         $(this).attr("value", "Play");
-//     }
-// });
-// function getAlias(sexType, age) {
-//     if (age < 12) {
-//         return sexType.$young[goRandom(sexType.$young.length)];
-//     } else if (age < 22) {
-//         return sexType.$teen[goRandom(sexType.$teen.length)];
-//     } else if (age < 40) {
-//         return sexType.$adult[goRandom(sexType.$adult.length)];
-//     } else {
-//         return sexType.$old[goRandom(sexType.$old.length)];
-//     }
-// }
+function htmlCommentProtection(text){
+    var newVal="";
+    for(var i=0;i<text.length;i++){
+        /*brutal statement*/
+        if((text.charAt(i)== "<"&&text.charAt(i+1)=="!")||(text.charAt(i)==">"&&text.charAt(i-1)=="-")||text.charAt(i)=="*"){
+            newVal+="";
+        }else{
+            newVal+=text.charAt(i);
+        }
+    }
+    return newVal;
+}
